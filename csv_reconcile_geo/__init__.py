a6103ef93c1569b012c3714943b1ddfc52090341
Douglas Mennella <douglas.mennella@gmail.com>
2021-04-16 10:27:45 +0900

    Bump version.  Re-add csv-reconcile dependency

--- csv_reconcile_geo/__init__.py
+++ csv_reconcile_geo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from csv_reconcile import scorer
 from geopy import distance
--- tests/test_csv_reconcile_geo.py
+++ tests/test_csv_reconcile_geo.py
@@ -2,4 +2,4 @@ from csv_reconcile_geo import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.2'
+    assert __version__ == '0.1.3'
